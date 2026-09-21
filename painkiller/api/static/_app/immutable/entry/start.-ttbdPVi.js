72af338b3078faa8619778cdbdf77467127b0aa1
Ricardo Borges <ricardoborges@gmail.com>
2026-09-21 15:36:58 -0300

    build: update static bundle with retractable SessionSidebar

--- painkiller/api/static/_app/immutable/entry/start.-ttbdPVi.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/zWCuSZJv.js";import{z as t}from"../chunks/BLf4zVT6.js";export{t as load_css,r as start};
---
+++ painkiller/api/static/_app/immutable/entry/start.Tx8-0v7w.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/C3YEubKp.js";import{z as t}from"../chunks/Dcy16yBN.js";export{t as load_css,r as start};
