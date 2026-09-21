48703f81e52f0d6163c715a1cf45d610c4372976
Ricardo Borges <ricardoborges@gmail.com>
2026-09-21 15:55:05 -0300

    build: update static bundle with stepper and backlog handoff

---
+++ painkiller/api/static/_app/immutable/entry/start.-m_lppB1.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/DXLCXOcu.js";import{z as t}from"../chunks/CsDXYbIx.js";export{t as load_css,r as start};
--- painkiller/api/static/_app/immutable/entry/start.B4Zj2Lsm.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/Cj6RYOGU.js";import{z as t}from"../chunks/DlDQ19Os.js";export{t as load_css,r as start};
