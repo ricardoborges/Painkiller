86e1dcaa36a5aa4c27718daf2f2da12933d0cc30
Ricardo Borges <ricardoborges@gmail.com>
2026-09-24 11:03:53 -0300

    feat config wizard

---
+++ painkiller/api/static/_app/immutable/entry/start.D4m64crf.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/Dzj8Df6r.js";import{z as t}from"../chunks/B_O1dxvr.js";export{t as load_css,r as start};
--- painkiller/api/static/_app/immutable/entry/start.DnIe1BnE.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/DG90gp_T.js";import{z as t}from"../chunks/D2YN-6WJ.js";export{t as load_css,r as start};
